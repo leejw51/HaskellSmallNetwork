199d66ae5b6a188d27dde34ae7b9ba5a1bcc5a9e
jongwhan lee <leejw51@gmail.com>
2019-05-28 17:14:47 +0900

    main

--- program/app/Main.hs
+++ program/app/Main.hs
@@ -2,5 +2,13 @@ module Main where
 
 import Lib
 
-main :: IO ()
-main = networkFunc
+
+main = do
+    --networkFun
+    putStrLn "Haskell Small Network" 
+    putStrLn "1. server" 
+    putStrLn "2. client"
+    putStrLn "q. exit"
+    w <- getLine
+    if w=="q" then return () else main
+    
