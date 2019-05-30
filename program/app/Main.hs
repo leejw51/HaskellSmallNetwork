f5bc1edbb88814a530b0e2a9167144798ff857ca
jongwhan lee <leejw51@gmail.com>
2019-05-30 14:32:21 +0000

    json test

--- program/app/Main.hs
+++ program/app/Main.hs
@@ -1,10 +1,15 @@
 module Main where
 
 import Lib
+import Test
 
 
 main = do
+    testFunc
+
+main2 = do
     --networkFun
+    putStrLn "-------------------------------------------------------------------"
     putStrLn "Haskell Small Network 2019.05.30" 
     putStrLn "1. server" 
     putStrLn "2. client"
